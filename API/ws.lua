38ccd7908f40e2dc573cc7f6b094ec86efc0b875
Virgile Hirtz <virgile.hirtz@gmail.com>
2021-12-06 18:10:23 +0100

    Correcting socket references

--- API/ws.lua
+++ API/ws.lua
@@ -5,12 +5,12 @@ function ws.ConnectSocket(ip)
     local err
     repeat
         ws.socket, err = http.websocket("ws://" .. ip .. ":80/ws")
-        if not socket then
+        if not ws.socket then
             print("Connection failed")
             print("Retrying in 10 seconds...")
             os.sleep(10)
         end
-    until socket
+    until ws.socket
     print("Connected to socket")
 end
 
@@ -26,7 +26,7 @@ function ws.Send(msg)
 end
 
 function ws.Receive()
-    local msg, bool = socket.receive(10)
+    local msg, bool = ws.socket.receive(10)
     if not msg then
         print("Error while waiting for socket")
         print("Rebooting in 10 seconds...")
